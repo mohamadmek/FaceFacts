8c285cbec6a6b96b57004968dc7587c8aefe6efd
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2024-11-11 00:05:38 +0100

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Mohamad Meksasi on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
